ac33815596677d95d73cd74c68b1c83ee4f605ab
Mickael Melkowski <m.melkowski@yahoo.fr>
2019-07-02 15:40:07 +0200

    add main

--- AsyncIO/asyncio.py
+++ AsyncIO/asyncio.py
@@ -18,3 +18,11 @@ async def count():
 
 async def main():
     await asyncio.gather(count(), count(), count())
+
+
+if __name__ == "__main__":
+    import time
+    s = time.perf_counter()
+    asyncio.run(main())
+    elapsed = time.perf_counter() - s
+    print(f"{__file__} executed in {elapsed:0.2f} seconds.")
